b65132f2bed843f3b13b2786580d9af00ebd6680
Youssef Called <42530663+youssefcalled@users.noreply.github.com>
2020-02-25 15:38:58 -0300

    Add files via upload

--- vendor/MF/Controller/Action.php
+++ vendor/MF/Controller/Action.php
@@ -1,1 +1,47 @@
+<?php
 
+namespace MF\Controller;
+
+abstract class Action{
+	
+	protected $view;
+
+
+	public function __construct(){
+
+		$this->view = new \stdClass();
+	}
+
+	protected function render($view,$layout = 'layout'){
+		
+		$this->view->page = $view;
+
+		//Se o layout existir, retorna o conteúdo da view dentro do layout
+		if(file_exists("../App/Views/".$layout.".phtml")){
+
+			require_once "../App/Views/".$layout.".phtml";
+
+		//Se não existir, retorna apenas o conteudo da view
+		}else{
+			$this->content();
+		}
+		
+	}
+
+	protected function content(){
+		//Pega o caminho da classe com o nome do controller
+		$classeAtual = get_class($this); 
+		//Se encontrar o caminho App\\Controllers\\, substitui para vazio, para ficar somente o nome do Controller Selecionado
+		$classeAtual = str_replace('App\\Controllers\\', '', $classeAtual);
+
+		//Pega somente o nome da pasta que contem as actions do Controller requisitado
+		$classeAtual = strtolower(str_replace('Controller', '', $classeAtual));
+
+		//echo $classeAtual;
+
+		require_once "../App/Views/".$classeAtual."/".$this->view->page.".phtml";
+	}
+
+}
+
+?>
